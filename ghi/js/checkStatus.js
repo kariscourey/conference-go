dea851a62388d0697abb442ccc1f6d5ca9507211
karis courey <kariscourey@gmail.com>
2022-10-13 21:59:38 -0400

    attemp checkstatus

---
+++ ghi/js/checkStatus.js
@@ -0,0 +1,35 @@
+// function getCookie(name) {
+//     const value = `; ${document.cookie}`;
+//     const parts = value.split(`; ${name}=`);
+//     if (parts.length === 2) return parts.pop().split(';').shift();
+//   }
+
+// // Get the cookie out of the cookie store
+// // const payloadCookie =  getCookie('jwt_access_payload'); // FINISH THIS
+// // const payloadCookie =  getCookie('jwt_access_payload'); // FINISH THIS
+// // const payloadCookie =  getCookie('jwt_access_payload'); //.get('jwt_access_payload'); // FINISH THIS
+// const payloadCookie =  cookieStore.get('jwt_access_payload').value; //.get('jwt_access_payload'); // FINISH THIS
+// console.log(Promise.resolve(payloadCookie));
+
+// // if (payloadCookie) {
+// //   // The cookie value is a JSON-formatted string, so parse it
+// //   const encodedPayload = JSON.parse(payloadCookie.value);
+// //   console.log(encodedPayload);
+
+// //   // Convert the encoded payload from base64 to normal string
+// //   const decodedPayload = // FINISH THIS
+
+// //   // The payload is a JSON-formatted string, so parse it
+// //   const payload = // FINISH THIS
+
+// //   // Print the payload
+// //   console.log(payload);
+
+// //   Check if "events.add_conference" is in the permissions.
+// //   If it is, remove 'd-none' from the link
+
+
+// //   Check if "events.add_location" is in the permissions.
+// //   If it is, remove 'd-none' from the link
+
+// // }
