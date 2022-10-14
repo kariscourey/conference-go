ebdba82b465650f8d2fe25a4282ee8e96233a478
karis courey <kariscourey@gmail.com>
2022-10-14 11:15:53 -0400

    add checkStatus functionality

--- ghi/js/checkStatus.js
+++ ghi/js/checkStatus.js
@@ -1,35 +1,46 @@
-// function getCookie(name) {
-//     const value = `; ${document.cookie}`;
-//     const parts = value.split(`; ${name}=`);
-//     if (parts.length === 2) return parts.pop().split(';').shift();
-//   }
+const getCookie = async (name) => {
+    const cookie = await cookieStore.get(name);
+    if (cookie) {
+        return cookie;
+    }
+    else {
+        console.log('cookie not found');
+    }
+  }
 
-// // Get the cookie out of the cookie store
-// // const payloadCookie =  getCookie('jwt_access_payload'); // FINISH THIS
-// // const payloadCookie =  getCookie('jwt_access_payload'); // FINISH THIS
-// // const payloadCookie =  getCookie('jwt_access_payload'); //.get('jwt_access_payload'); // FINISH THIS
-// const payloadCookie =  cookieStore.get('jwt_access_payload').value; //.get('jwt_access_payload'); // FINISH THIS
-// console.log(Promise.resolve(payloadCookie));
+// Get the cookie out of the cookie store
+const payloadCookie = await getCookie('jwt_access_payload'); //.get('jwt_access_payload'); // FINISH THIS
+// console.log(payloadCookie);
 
-// // if (payloadCookie) {
-// //   // The cookie value is a JSON-formatted string, so parse it
-// //   const encodedPayload = JSON.parse(payloadCookie.value);
-// //   console.log(encodedPayload);
+if (payloadCookie) {
+  // The cookie value is a JSON-formatted string, so parse it
+  const encodedPayload = JSON.parse(payloadCookie.value);
+//   console.log(encodedPayload);
 
-// //   // Convert the encoded payload from base64 to normal string
-// //   const decodedPayload = // FINISH THIS
+  // Convert the encoded payload from base64 to normal string
+  const decodedPayload = atob(encodedPayload) // FINISH THIS
+//   console.log(decodedPayload);
 
-// //   // The payload is a JSON-formatted string, so parse it
-// //   const payload = // FINISH THIS
+  // The payload is a JSON-formatted string, so parse it
+  const payload = JSON.parse(decodedPayload) // FINISH THIS
 
-// //   // Print the payload
-// //   console.log(payload);
+  // Print the payload
+//   console.log(payload);
 
 // //   Check if "events.add_conference" is in the permissions.
 // //   If it is, remove 'd-none' from the link
+    if (payload.user.perms['events.add_conference']) {
+        var new_conf = document.getElementById("new_conf");
+        var new_pres = document.getElementById("new_pres");
+        new_conf.classList.remove("d-none");
+        new_pres.classList.remove("d-none");
+    }
 
 
 // //   Check if "events.add_location" is in the permissions.
 // //   If it is, remove 'd-none' from the link
-
-// // }
+    if (payload.user.perms['events.add_location']) {
+        var new_loc = document.getElementById("new_loc");
+        new_loc.classList.remove("d-none");
+    }
+}
